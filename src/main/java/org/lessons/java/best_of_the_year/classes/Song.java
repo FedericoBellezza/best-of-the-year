86505392e5a0cae8c26f79c349dd761eac99c056
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-03-18 19:04:33 +0100

    progress

---
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
@@ -0,0 +1,39 @@
+package org.lessons.java.best_of_the_year.classes;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String title;
+
+    public Song(){}
+
+    public Song(int id, String title){
+        this.title = title;
+        this.id = id;
+    }
+
+    // getters
+    public String getTitle(){
+        return title;
+    }
+    public int getId(){
+        return id;
+    }
+
+    // setters
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    // to string
+    @Override
+    public String toString() {
+        return "Id: " + id + ", Titolo: " + title;
+    }
+
+
+}
--- src/main/java/org/lessons/java/best_of_the_year/controller.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller.java
@@ -1,20 +1,74 @@
 package org.lessons.java.best_of_the_year;
 
+import java.util.ArrayList;
+
+import org.lessons.java.best_of_the_year.classes.Movie;
+import org.lessons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 @RequestMapping("/")
 public class controller {
 
+    
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("name", "Federico" );
         return "home";
     }
+
+    @GetMapping("/movies")
+    public String bestMovies(Model model) {
+        model.addAttribute("listaDeiMiglioriFilm", getBestMovies());
+        return "movies";
+
+    }
+    @GetMapping("/songs")
+    public String bestSongs(Model model){
+        model.addAttribute("listaDelleMiglioriCanzoni", getBestSongs());
+        return "songs";
+    }
+
+    @GetMapping("/movies/{id}")
+    public String movieById(Model model, @PathVariable("id") String urlContent){
+        model.addAttribute("id", urlContent);
+        return "pageById";
+    }
+
+
+
     
     
+    // getters
+    private ArrayList<Movie> getBestMovies(){
+        Movie movieUno = new Movie(1, "Harry Potter");
+        Movie movieDue = new Movie(2, "Il Signore degli Anelli");
+        Movie movieTre = new Movie(3, "Annabelle");
+
+        ArrayList<Movie> listaDeiMiglioriFilm = new ArrayList<>();
+        listaDeiMiglioriFilm.add(movieUno);
+        listaDeiMiglioriFilm.add(movieDue);
+        listaDeiMiglioriFilm.add(movieTre);
+
+        return listaDeiMiglioriFilm;
+    }
+    private ArrayList<Song> getBestSongs(){
+        Song songUno = new Song(1, "Balorda Nostalgia");
+        Song songDue = new Song(2, "Volevo essere un duro");
+        Song songTre = new Song(3, "La Cura");
+
+        ArrayList<Song> listaDelleMiglioriCanzoni = new ArrayList<>();
+        listaDelleMiglioriCanzoni.add(songUno);
+        listaDelleMiglioriCanzoni.add(songDue);
+        listaDelleMiglioriCanzoni.add(songTre);
+
+        return listaDelleMiglioriCanzoni;
+    }
 }
